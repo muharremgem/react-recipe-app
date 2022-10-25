fb0f030c502d90ecf1ff347e34d8206991fe6804
muharrem gem <muharremgem28@gmail.com>
2022-10-25 20:00:58 +0300

    commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,12 +5,12 @@ import meal from "../assets/header.png";
 const Login = () => {
   return (
     <div>
-      <h1 className="site_heading">Food House</h1>
-      <h2 className="site_subheading">
-        Login to See All of the Delicious Recipes
-      </h2>
       <div className="meal">
         <form className="log">
+          <h1 className="site_heading">Food House</h1>
+          <h2 className="site_subheading">
+            Login to See All of the Delicious Recipes
+          </h2>
           <input type="text" name="name" required placeholder="Your name" />
           <input
             type="password"
