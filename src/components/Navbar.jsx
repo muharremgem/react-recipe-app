79498c6681b8e53c3bcf26d66a4796858173450e
muharrem gem <muharremgem28@gmail.com>
2022-10-27 20:18:36 +0300

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,14 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div>
-      <div>
-        <h1>Food House</h1>
-        <a href="">About</a>
-        <a href="">Logout</a>
-        <Link to="/">Github</Link>
+      <div className="bg-slate-500 flex justify-between cursor-pointer items-end py-5 z-10 w-full ">
+        <h1 className="ml-16 ">Food House</h1>
+        <div className=" flex gap-5  mr-10 pr-5  ">
+          <Link to="/home">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/">Logout</Link>
+          <Link to="/">Logout</Link>
+        </div>
       </div>
     </div>
   );
