a8f32cd91d786d1fbbb45981dc39b54456e0fa62
muharrem gem <muharremgem28@gmail.com>
2022-10-27 20:45:28 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Login from "./pages/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/Home" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,30 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div>
-      <div className="bg-green-500 text-white font-bold text-xl flex justify-between cursor-pointer items-end py-5 z-10 w-full ">
-        <h1 className="ml-16 ">Food House</h1>
+      <div className="bg-green-500 font-bold text-xl flex justify-between cursor-pointer items-end py-5 z-10 w-full ">
+        <NavLink
+          className="ml-16"
+          to="/home"
+          style={({ isActive }) => ({
+            color: isActive ? "white" : "",
+          })}
+        >
+          Food House
+        </NavLink>
+
         <div className=" flex gap-5  mr-10 pr-5  ">
-          <Link to="/home">Home</Link>
+          <NavLink
+            className="hover:text-blue-400 transition ease-in-out delay-100 hover:scale-125 "
+            to="/Home"
+            style={({ isActive }) => ({
+              color: isActive ? "white" : "",
+            })}
+          >
+            Home
+          </NavLink>
           <Link to="/about">About</Link>
           <Link to="/">Logout</Link>
         </div>
