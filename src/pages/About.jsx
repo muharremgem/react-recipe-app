fc69c2405a2e19eb417a9d17364a214a97a04d1e
muharrem gem <muharremgem28@gmail.com>
2022-10-28 18:44:10 +0300

    react ıcons

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,28 +6,28 @@ import { SiGithub, SiLinkedin } from "react-icons/si";
 
 const About = () => {
   return (
-    <div>
+    <div className="bg-blue-300">
       <Navbar />
-      <div className=" bg-blue-300 w-full h-screen flex flex-col ">
+      <div className="  w-full h-screen flex flex-col ">
         <img className="w-full" src={testi} alt="about" />
         <p className="flex justify-center text-3xl font-bold mt-2">
           Lezzetli Yemek Tarifleri İçin Lütfen Bize Ulaşın...=)
         </p>
         <a
           href="https://github.com/muharremgem"
-          className="flex flex-wrap justify-center"
+          className="flex justify-center"
           target="_blank"
         >
           <SiGithub className="text-5xl mt-5 font-bold" />
         </a>
         <a
           href="https://www.linkedin.com/in/muharremgem/"
-          className="flex flex-wrap justify-center"
+          className="flex justify-center"
           target="_blank"
         >
-          <SiLinkedin className="text-5xl mt-5 font-bold" />
+          <SiLinkedin className="text-5xl  mt-5 font-bold" />
         </a>
-        <img className="w-full  mt-20" src={offer} alt="about" />
+        <img className="w-full  mt-5" src={offer} alt="about" />
       </div>
     </div>
   );
