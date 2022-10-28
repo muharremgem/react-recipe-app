62b00a0849599a1ce1d8f710b28118c6eb1e95c1
muharrem gem <muharremgem28@gmail.com>
2022-10-28 18:38:32 +0300

    about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import testi from "../assets/testimonial.png";
 import offer from "../assets/offer.png";
-import { SiGithub } from "react-icons/si";
+import { SiGithub, SiLinkedin } from "react-icons/si";
 
 const About = () => {
   return (
@@ -12,9 +12,22 @@ const About = () => {
         <img className="w-full" src={testi} alt="about" />
         <p className="flex justify-center text-3xl font-bold mt-2">
           Lezzetli Yemek Tarifleri İçin Lütfen Bize Ulaşın...=)
-          <a href={SiGithub}>{SiGithub}</a>
         </p>
-        <img className="w-full  mt-36" src={offer} alt="about" />
+        <a
+          href="https://github.com/muharremgem"
+          className="flex flex-wrap justify-center"
+          target="_blank"
+        >
+          <SiGithub className="text-5xl mt-5 font-bold" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/muharremgem/"
+          className="flex flex-wrap justify-center"
+          target="_blank"
+        >
+          <SiLinkedin className="text-5xl mt-5 font-bold" />
+        </a>
+        <img className="w-full  mt-20" src={offer} alt="about" />
       </div>
     </div>
   );
