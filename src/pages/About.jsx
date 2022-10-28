5f2cf9d5e145ea342deb047e2b68c1bc871b01c1
muharrem gem <muharremgem28@gmail.com>
2022-10-28 18:28:26 +0300

    commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import testi from "../assets/testimonial.png";
 import offer from "../assets/offer.png";
+import { SiGithub } from "react-icons/si";
 
 const About = () => {
   return (
@@ -10,9 +11,8 @@ const About = () => {
       <div className=" bg-blue-300 w-full h-screen flex flex-col ">
         <img className="w-full" src={testi} alt="about" />
         <p className="flex justify-center text-3xl font-bold mt-2">
-          Lezzetli Yemek Tarifleri İçin Lütfen Bize Ulaşın...
-
-          <a href=""></a>
+          Lezzetli Yemek Tarifleri İçin Lütfen Bize Ulaşın...=)
+          <a href={SiGithub}>{SiGithub}</a>
         </p>
         <img className="w-full  mt-36" src={offer} alt="about" />
       </div>
