66529559f80cc4fe9f4b51930524d97019223e1d
muharrem gem <muharremgem28@gmail.com>
2022-10-28 14:15:54 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Details from "./pages/Details";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/details" element={<Details />} />
       </Routes>
     </BrowserRouter>
   );
